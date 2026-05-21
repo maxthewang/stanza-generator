ed4a28c51021a6b22c2d0b1416754835906f4019
maxthewang <maxthe.wang@mail.utoronto.ca>
2026-05-21 16:21:05 -0400

    Logo, tab title, python Ruff formatting

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
+import os
+
+import duckdb
 from datasets import load_dataset
 from flask import Flask, jsonify, render_template
-import duckdb
-import os
 
 app = Flask(__name__)
 
-dataset = load_dataset(
-    "biglam/gutenberg-poetry-corpus",
-    split="train"
-)
+dataset = load_dataset("biglam/gutenberg-poetry-corpus", split="train")
 
 duckdb.register("train", dataset.data.table)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/stanza")
 def stanza():
@@ -25,15 +25,10 @@ def stanza():
         LIMIT 4
     """).fetchall()
 
-    return jsonify({
-        "rows": [
-            {
-                "line": row[0],
-                "gid": row[1]
-            }
-            for row in results
-        ]
-    })
+    return jsonify(
+        {"rows": [{"line": row[0], "gid": row[1]} for row in results]}
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
